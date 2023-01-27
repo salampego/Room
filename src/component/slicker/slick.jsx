12701c833e3c3787da2aabb767cfac174c71dde0
Haik <55463651+salampego@users.noreply.github.com>
2023-01-27 14:49:59 +0200

    /

--- src/component/slicker/slick.jsx
+++ src/component/slicker/slick.jsx
@@ -1,5 +1,7 @@
-import "./slick.css";
 import Slider from "react-slick";
+import { useRef } from "react";
+
+import "./slick.css";
 import {
   imgHeroJpg,
   imgHeroWebp,
@@ -14,17 +16,18 @@ import {
 } from "../../images";
 import { ReactComponent as ArrowLeft } from "../../svg/slick/arrowLeft.svg";
 import { ReactComponent as ArrowRight } from "../../svg/slick/arrowRight.svg";
-import { useRef } from "react";
 
 const settings = {
   arrows: false,
   dots: true,
   infinite: true,
-  speed: 500,
+  centerMode: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  centerMode: true,
   variableWidth: true,
+  adaptiveHeight: true,
+  appendDots: (dots) => <ul className="slick__dots--list">{dots}</ul>,
+  customPaging: () => <div className="slick__dots--item"></div>,
 };
 
 export const Slick = () => {
@@ -34,7 +37,7 @@ export const Slick = () => {
     <section className="section">
       <div className="slick__container container">
         <Slider ref={arrowRef} {...settings}>
-          <div style={{ width: 1200 }}>
+          <div>
             <picture>
               <source
                 srcSet={`${imgSlicker1} 1x, ${imgSlicker1_2x} 2x`}
@@ -43,36 +46,49 @@ export const Slick = () => {
               <source
                 srcSet={`${imgSlicker1Webp} 1x, ${imgSlicker1Webp2x} 2x`}
               />
-              <img src="../../images/hero/hero.jpg" alt="" />
+              <img
+                src="../../images/hero/hero.jpg"
+                alt=""
+                className="slick__picture"
+              />
               <source />
             </picture>
           </div>
-          <div style={{ width: 1200 }}>
+          <div>
             <picture>
               <source
                 srcSet={`${imgHeroWebp} 1x, ${imgHeroWebp2x} 2x`}
                 type="image/webp"
               />
               <source srcSet={`${imgHeroJpg} 1x, ${imgHeroJpg2x} 2x`} />
-              <img src="../../images/hero/hero.jpg" alt="" />
+              <img
+                src="../../images/hero/hero.jpg"
+                alt=""
+                className="slick__picture"
+              />
               <source />
             </picture>
           </div>
-          <div style={{ width: 1200 }}>
+          <div>
             <picture>
               <source
                 srcSet={`${imgHeroWebp} 1x, ${imgHeroWebp2x} 2x`}
                 type="image/webp"
               />
               <source srcSet={`${imgHeroJpg} 1x, ${imgHeroJpg2x} 2x`} />
-              <img src="../../images/hero/hero.jpg" alt="" />
+              <img
+                src="../../images/hero/hero.jpg"
+                alt=""
+                className="slick__picture"
+              />
               <source />
             </picture>
           </div>
         </Slider>
+
         <div className="slick__arrow">
           <button
-            onClick={() => arrowRef.current.slickPrev()}
+            onClick={() => arrowRef.current.slickNext()}
             className="slick__arrow-btn back-btn"
             type="button"
           >
